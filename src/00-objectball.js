c8c1cdccfd6995ba8adedc3f9b45aa92317aa28a
Xylon <xyfullns@gmail.com>
2023-01-26 00:20:01 -0500

    Objects

---
+++ src/00-objectball.js
@@ -0,0 +1,32 @@
+function gameObject() {
+  const gameObject = {
+    home: {
+      teamName: "",
+      colors: [],
+      players: {
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slamDunks: "",
+      },
+    },
+    away: {
+      teamName: "",
+      colors: [],
+      players: {
+        number: "",
+        shoe: "",
+        points: "",
+        rebounds: "",
+        assists: "",
+        steals: "",
+        blocks: "",
+        slamDunks: "",
+      },
+    },
+  };
+}
